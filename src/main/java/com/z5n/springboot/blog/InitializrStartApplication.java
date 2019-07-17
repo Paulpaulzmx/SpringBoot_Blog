c5fc1ffdfd15492e4807e0e73622bec4bc69a3b1
Paulpaulzmx <980455605@qq.com>
2019-07-17 10:19:58 +0800

    Initial commit

---
+++ src/main/java/com/z5n/springboot/blog/InitializrStartApplication.java
@@ -0,0 +1,13 @@
+package com.z5n.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrStartApplication.class, args);
+    }
+
+}
